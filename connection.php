e5a713b32374d09eda0dc93917b02fef452191aa
Sandor Kis <kisandor@hotmail.com>
2022-12-17 22:12:07 +0000

    database connection class has been implemented

--- connection.php
+++ connection.php
@@ -1,7 +1,22 @@
 <?php
+Class dbObj{
+ /* Database connection */
+  var $servername = "localhost";
+  var $username = "root";
+  var $password = "";
+  var $dbname = "cars";
+  var $conn;
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+  function getConnstring() {
+    $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
+      
+    /* check connection */
+    if (mysqli_connect_errno()) {
+       printf("Connect failed: %s\n", mysqli_connect_error());
+       exit();
+    } else {
+       $this->conn = $con;
+      }
+      return $this->conn;
+    }
+  }
